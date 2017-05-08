10eb56daafd252ca66dab72d38bba405767c2f0a
Debasish Banerjee <cygnus2@gmail.com>
2017-05-08 13:38:15 +0200

    Failed attempts to remove NA

--- sberbank.m
+++ sberbank.m
@@ -9,48 +9,53 @@ pkg load io
 % load raw training data
 raw = csv2cell('../train.csv');
 % it seems like there are some 'NA' fields which screw up the analysis
-% replacing them by 0
-raw(strcmp('NA',raw))={0};
-% size of training set
-m = size(raw,1) - 1; 
+% replacing them by -99 to remove them later
+ raw(strcmp('NA',raw))={-99};
+ 
+% remove the NA features 
+m = size(raw,1); 
+X = [ raw{[2:m],[3:5]}];
+X = reshape(X, size(X,2)/3, 3);
+
+
+
+
 %number of features
-mf = size(raw,2);
+%mf = size(raw,2);
 
 % try a simple linear regression model using the features
 % full_sq, life_sq and floor
-id = [ raw{[2:m+1],1} ];
+%id = [ raw{[2:m+1],1} ];
 
-x = [ raw{[2:m+1],[3:5]}];
-x = reshape(x, size(x,2)/3, 3);
-y = [ raw{[2:m+1],mf} ];
+%y = [ raw{[2:m+1],mf} ];
 
 % plot a histogram, just for fun
-plotData(id, y)
+%plotData(id, y)
 
 % Scale features and set them to zero mean
-fprintf('Normalizing Features ...\n');
+%fprintf('Normalizing Features ...\n');
 
-[x mu sigma] = featureNormalize(x);
-x = [ones(m, 1), x]; % Add a column of ones to x
+%[x mu sigma] = featureNormalize(x);
+%x = [ones(m, 1), x]; % Add a column of ones to x
 
 % running gradient descent
-fprintf('Running gradient descent ...\n');
+%fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.1;
-num_iters = 400;
+%alpha = 0.1;
+%num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
-theta = zeros(4, 1);
-[theta, J_history] = gradientDescentMulti(x, y, theta, alpha, num_iters);
+%theta = zeros(4, 1);
+%[theta, J_history] = gradientDescentMulti(x, y, theta, alpha, num_iters);
 
 % Plot the convergence graph
-figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
-xlabel('Number of iterations');
-ylabel('Cost J');
+%figure;
+%plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+%xlabel('Number of iterations');
+%ylabel('Cost J');
 
 % Display gradient descent's result
-fprintf('Theta computed from gradient descent: \n');
-fprintf(' %f \n', theta);
-fprintf('\n');
+%fprintf('Theta computed from gradient descent: \n');
+%fprintf(' %f \n', theta);
+%fprintf('\n');
